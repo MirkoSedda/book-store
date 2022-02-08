4e0cc03676816dca7e362991a72c080a265538a3
yourusername <kunay69@hotmail.it>
2022-02-08 17:14:26 +0100

    footer done

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,7 +2,7 @@
 const MyFooter = () => (
     
    <footer className="d-flex bg-dark fixed-bottom">
-      <h3 className="text-light m-auto p-3">Strive Books - Copyright 2022 </h3>
+      <h5 className="text-light m-auto p-3">Strive Books - Copyright 2022 </h6>
     </footer>
 )
     
