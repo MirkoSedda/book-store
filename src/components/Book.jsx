92eb571d4a0b65c636037cebfc580e04255c1c6a
yourusername <kunay69@hotmail.it>
2022-02-09 17:51:05 +0100

    project done

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,20 +1,36 @@
+import React, { Component } from 'react'
 import { Col } from 'react-bootstrap'
 import MyBadge from './MyBadge'
 import './Book.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const Book = ({ bookData }) => {
-  return (<Col xs={12} md={3} className="m-1 p-1">
-    <div className="position-relative">
-    <MyBadge branding='Delete Book' color='dark' className="position-absolute"/>
-    <img
-      className="d-block img-fluid img"
-      src={bookData.img}
-      alt={bookData.title}
-      />
-      <h4 className='text-light'>{bookData.title}</h4>
-      </div>
-  </Col>)
-}
+export default class Book extends Component {
 
-export default Book
+  state = {
+    selected: false,
+  }
+
+  handleClick = () => { this.setState({ selected: !this.state.selected }) }
+
+
+  render() {
+    return (
+      <Col
+        xs={12}
+        md={3}
+        className="m-1 p-1 card-container"
+      >
+        <div className="card bg-dark" onClick={this.handleClick}>
+
+          <MyBadge branding="Delete Book" color="dark" className="badge" />
+          <img
+            className="d-block img-fluid img"
+            src={this.props.bookData.img}
+            alt={this.props.bookData.title}
+          />
+          <h4 className="bg-dark text-light">{this.props.bookData.title}</h4>
+        </div>
+      </Col>
+    )
+  }
+}
--- src/components/BookWrapper.jsx
+++ src/components/BookWrapper.jsx
@@ -1,23 +1,45 @@
-import { Container, Row } from 'react-bootstrap'
+import { Component } from 'react'
+import { InputGroup, FormControl, Container, Row } from 'react-bootstrap'
+//import MyInput from './MyInput'
+
 import Book from './Book'
- 
 import books from '../data/horror.json'
 
+export default class BookWrapper extends Component {
+  state = {
+    bookTitle: '',
 
-const BookWrapper = () => {
-        return      (<Container>
-                        <Row>
-                           {books.map((book) => { return <Book bookData={book} key={book.asin} /> })}
-                        </Row>
-                    </Container>)
+  }
+
+
+  handleChange = value => {
+    this.setState({ bookTitle: value })
+  }
+
+  render() {
+    return (
+      <Container>
+        <>
+          <InputGroup size="sm" className="mb-3">
+            <FormControl
+              onChange={e => this.handleChange(e.target.value)}
+              aria-label="Small"
+              aria-describedby="inputGroup-sizing-sm"
+            />
+          </InputGroup>
+        </>
+        <Row>
+          {books
+            .filter(book =>
+              book.title
+                .toLowerCase()
+                .includes(this.state.bookTitle.toLowerCase())
+            )
+            .map(book => {
+              return <Book bookData={book} key={book.asin} />
+            })}
+        </Row>
+      </Container>
+    )
+  }
 }
-
-export default BookWrapper
-
-
-
-
-
-
-
-
